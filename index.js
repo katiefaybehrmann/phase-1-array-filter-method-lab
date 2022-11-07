fe7a573995464e4f217151e91d851363470c186b
Katie <katiefaybehrmann@gmail.com>
2022-11-07 14:10:56 -0700

    commit tree 995bd73b0b2fb721b9e04072a009e6098e1e2ce7

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution here
+function findMatching(drivers, name){
+    return drivers.filter(x => x.toLowerCase() === name.toLowerCase())
+}
+
+function fuzzyMatch(drivers, letters){
+    return drivers.filter(function (x){
+        return x.startsWith(letters)
+    })
+}
+
+function matchName(drivers, names){
+    return drivers.filter(function (x){
+        return x.name === names;
+    })
+}
+
